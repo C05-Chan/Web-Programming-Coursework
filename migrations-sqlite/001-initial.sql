211a3344c9d2d2336f60997bfc3807213853a34b
Crystal Chan <crystal.chan2607@gmail,com>
2025-05-09 17:27:42 +0100

    first commit

---
+++ migrations-sqlite/001-initial.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS race_data (
+  raceData_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  data_type TEXT NOT NULL CHECK (data_type IN ('times', 'runners')),
+  data_array TEXT NOT NULL,
+  client_id INT NOT NULL,
+  time DATETIME,
+
+  UNIQUE(client_id, data_type)
+);
+
+CREATE TABLE IF NOT EXISTS race_results (
+    id INTEGER PRIMARY KEY CHECK (id = 1),
+    times_array TEXT NOT NULL DEFAULT '[]',
+    runners_array TEXT NOT NULL DEFAULT '[]',
+    time DATETIME
+);
